6dc5109807a0815e8d505aaddf2d44777385a071
Sergey Melekhin <sergey@emply.ru>
2014-12-01 17:52:55 +1000

    First version

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name = "nosemerge",
+    version = "0.1",
+    packages = find_packages(),
+    scripts = ['nosemerge.py'],
+
+    install_requires = [],
+  
+
+    package_data = {
+        '': ['*.ini', '*.txt'],
+    },
+
+    # metadata for upload to PyPI
+    author = "Sergey Melekhin",
+    author_email = "cpro29a@gmail.com",
+    description = "Simple nosetests xml report merge tool",
+    license = "LGPL",
+    keywords = "xml unittest nose nosetests",
+
+)
